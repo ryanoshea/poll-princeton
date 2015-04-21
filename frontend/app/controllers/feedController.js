c02f867f8d7fc1cbd2ff337cba5d8ae7f72b4235
Ryan O'Shea <ryan@ryanoshea.com>
2015-04-21 11:36:12 -0400

    Can now change feed sort from frontend

--- frontend/app/controllers/feedController.js
+++ frontend/app/controllers/feedController.js
@@ -9,10 +9,11 @@ cont.controller('feedController', function ($scope, $filter, $http, $location) {
   $('body').css('padding-top', '50px');
 
   $scope.fetchedDevs = false;
-  $scope.devs = [];
   $scope.fetchedPolls = false;
   $scope.polls = [];
   $scope.currentPolls = 0;
+  $scope.sort = "popular";
+
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   $scope.fetchDevs = function () {
@@ -39,13 +40,13 @@ cont.controller('feedController', function ($scope, $filter, $http, $location) {
     });
   };
 
-  //Get call returns an array of objects containing three things: pollData (a complete poll with all data), 
+  //Get call returns an array of objects containing three things: pollData (a complete poll with all data),
   //userVote (true for up, false for down) and userResponse (however Ryan implemented it. -1 for no response I think)
   $scope.fetch10Best = function () {
     var user = localStorage.getItem('netid');
     $http.get('http://' + window.location.hostname + '/ppapi/polls/get/popular/' + user + '/' + $scope.currentPolls).success(function (data, status, headers, config) {
       $scope.polls = data;
-      $scope.currentPolls = $scope.currentPolls + 10; 
+      $scope.currentPolls = $scope.currentPolls + 10;
       for (var i in $scope.polls) {
         var thisPoll = $scope.polls[i].pollData;
         var date = new Date(thisPoll.time);
@@ -59,7 +60,7 @@ cont.controller('feedController', function ($scope, $filter, $http, $location) {
     var user = localStorage.getItem('netid');
     $http.get('http://' + window.location.hostname + '/ppapi/polls/get/newest/' + user + '/' + $scope.currentPolls).success(function (data, status, headers, config) {
       $scope.polls = data;
-      $scope.currentPolls = $scope.currentPolls + 10; 
+      $scope.currentPolls = $scope.currentPolls + 10;
       for (var i in $scope.polls) {
         var thisPoll = $scope.polls[i].pollData;
         var date = new Date(thisPoll.time);
@@ -85,21 +86,31 @@ cont.controller('feedController', function ($scope, $filter, $http, $location) {
     });
   };*/
 
-  //async?
-  $scope.fetch10Best();
-
-
-
-  /*$scope.showComposeFlyout = function () {
-    $('html').css('overflow', 'hidden');
-    $('.flyout-compose').fadeIn('fast');
-    $('.flyout-compose-contents').show('fast');
+  $scope.sortPopular = function () {
+    if ($scope.sort !== 'popular') {
+      $('.polls').fadeOut(function () {
+        $scope.sort = 'popular';
+        $scope.polls = [];
+        $scope.currentPolls = 0;
+        $scope.fetch10Best();
+        $('.polls').fadeIn();
+      });
+    }
   };
-  $scope.hideComposeFlyout = function () {
-    $('.flyout-compose').fadeOut('fast');
-    $('.flyout-compose-contents').hide('fast');
-    $('html').css('overflow', 'auto');
-  };*/
+
+  $scope.sortNewest = function () {
+    if ($scope.sort !== 'newest') {
+      $('.polls').fadeOut(function () {
+        $scope.sort = 'newest';
+        $scope.polls = [];
+        $scope.currentPolls = 0;
+        $scope.fetch10New();
+        $('.polls').fadeIn();
+      });
+    }
+  };
+
+  $scope.fetch10Best();
 });
 
 })();
