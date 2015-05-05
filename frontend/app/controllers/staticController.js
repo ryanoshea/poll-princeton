a383aa31871d46174258207bb619d6bb0bb3eff1
tessmarchant <tessmmarchant@gmail.com>
2015-05-05 01:16:47 -0400

    Fixed compose button on public pages
    And a little layout tweak

--- frontend/app/controllers/staticController.js
+++ frontend/app/controllers/staticController.js
@@ -4,7 +4,7 @@ var cont = angular.module('poll-princeton.controllers');
 
 cont.controller('staticController', function ($scope, $filter, $http, $location) {
 
-  
+  $('body').css('padding-top', '50px');
 
 });
 
