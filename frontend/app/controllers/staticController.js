975e4aae531a049ae262355ade72e2c3b0ec1e11
Ryan O'Shea <ryan@ryanoshea.com>
2015-05-05 16:20:32 -0400

    scrolling fix for static pages

--- frontend/app/controllers/staticController.js
+++ frontend/app/controllers/staticController.js
@@ -5,6 +5,7 @@ var cont = angular.module('poll-princeton.controllers');
 cont.controller('staticController', function ($scope, $filter, $http, $location) {
 
   $('body').css('padding-top', '50px');
+  window.scrollTo(0,0);
 
 });
 
